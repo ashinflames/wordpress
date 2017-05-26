000b310bd78214b6b93bc52e3461596d468357a3
Ваше Имя <you@example.com>
2017-05-26 16:07:09 +0300

    vcxz

--- wp-config.php
+++ wp-config.php
@@ -38,7 +38,7 @@ define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
 /**#@+
- * Authentication Unique Keys and Salts.
+ * Authentication U nique Keys and Salts.
  *
  * Change these to different unique phrases!
  * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
